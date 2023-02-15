92ebdddff022e7773d03a6a97067d56d26dc2b75
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-14 22:28:58 -0300

    feat: add get users use-case

---
+++ src/controllers/useCases/getUsers/GetUsersUseCase.ts
@@ -0,0 +1,13 @@
+import { IUser } from "@/models/IUser"
+import { IGetUsersRepository } from "@/repositories/user/interfaces/IGetUsersRepository"
+
+export class GetUserUseCase {
+  constructor(
+    private getUsersRepository: IGetUsersRepository
+  ) {}
+
+  async execute(): Promise<IUser[]> {
+    const users = await this.getUsersRepository.findAll()
+    return users
+  }
+}
