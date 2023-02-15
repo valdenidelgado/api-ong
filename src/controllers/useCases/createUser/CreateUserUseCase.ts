b84d9bbad0610a38d0d80b9db55dfc756065f08d
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-14 23:02:37 -0300

    fix: add correct params to createUser on CreateUserUseCase

--- src/controllers/useCases/createUser/CreateUserUseCase.ts
+++ src/controllers/useCases/createUser/CreateUserUseCase.ts
@@ -1,14 +1,18 @@
 import { ICreateUserRepository } from "@/repositories/user/interfaces/ICreateUserRepository"
 import { ICreateUserParams } from "@/repositories/user/interfaces/helpers/ICreateUserParams"
 import { IUser } from "../../../models/IUser"
-import { HttpRequest } from "@/controllers/helpers/protocols"
-import { serverError } from "@/controllers/helpers/helpers"
 
 export class CreateUserUseCase {
   constructor(private readonly createUserRepository: ICreateUserRepository) {}
 
-  async execute(httpRequest: HttpRequest<ICreateUserParams>): Promise<IUser> {
-    const user = await this.createUserRepository.createUser(httpRequest.body!)
+  async execute(param: ICreateUserParams): Promise<IUser> {
+    const { name, email, password } = param
+
+    const user = await this.createUserRepository.createUser({
+      name,
+      email,
+      password,
+    })
     return user
   }
 }
