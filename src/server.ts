e4f34e07123f4de9b009130cd5bb688ca3189514
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-14 23:09:57 -0300

    feat: add config server por from .env for start server and add middleware '/users'

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import express from "express"
 import conn from "./db/conn"
 import router from "./routes/user.routes"
+import { config } from "./config/config"
 
 const app = express()
-
-const port = 8080
 
 conn()
 
 app.use(express.json())
-app.use(router)
+app.use("/users", router)
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(config.server.port, () => {
+  console.log(`Server running on port ${config.server.port}`)
 })
