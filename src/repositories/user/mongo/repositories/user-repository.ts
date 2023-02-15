ca02147abc4bf7eebba7feedfb2fc0bbadaea4c0
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-14 22:12:04 -0300

    feat: add findAll to mongo user repository

--- src/repositories/user/mongo/repositories/user-repository.ts
+++ src/repositories/user/mongo/repositories/user-repository.ts
@@ -2,8 +2,14 @@ import { IUser } from "@/models/IUser";
 import { ICreateUserRepository } from "../../interfaces/ICreateUserRepository";
 import { ICreateUserParams } from "../../interfaces/helpers/ICreateUserParams";
 import UserModel from "../models/UserModel";
+import { IGetUsersRepository } from "../../interfaces/IGetUsersRepository";
 
-export class UserRepository implements ICreateUserRepository {
+export class UserRepository implements ICreateUserRepository, IGetUsersRepository {
+  async findAll(): Promise<IUser[]> {
+    const users = await UserModel.find();
+
+    return users;
+  }
   async createUser(params: ICreateUserParams): Promise<IUser> {
     const user = await UserModel.create(params);
 
