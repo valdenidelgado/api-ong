38909c054b8db1fba19905c238c73401b68a03b1
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-14 23:04:33 -0300

    fix: add correct request to CreateUserController

--- src/controllers/useCases/createUser/CreateController.ts
+++ src/controllers/useCases/createUser/CreateController.ts
@@ -1,19 +1,21 @@
-import { Request } from "express"
 import { IUser } from "../../../models/IUser"
-import { HttpResponse } from "@/controllers/helpers/protocols"
+import { HttpRequest, HttpResponse } from "@/controllers/helpers/protocols"
 import { created } from "@/controllers/helpers/helpers"
 import { UserRepository } from "../../../repositories/user/mongo/repositories/user-repository"
 import { CreateUserUseCase } from "./CreateUserUseCase"
 import { serverError } from "../../helpers/helpers"
+import { ICreateUserParams } from "@/repositories/user/interfaces/helpers/ICreateUserParams"
 
 const userRepository = new UserRepository()
 
 export class CreateUserController {
-  async handle(request: Request): Promise<HttpResponse<IUser | string>> {
+  async handle(
+    request: HttpRequest<ICreateUserParams>
+  ): Promise<HttpResponse<IUser | string>> {
     try {
       const createUserUseCase = new CreateUserUseCase(userRepository)
 
-      const user = await createUserUseCase.execute(request.body)
+      const user = await createUserUseCase.execute(request.body!)
 
       return created(user)
     } catch (error) {
