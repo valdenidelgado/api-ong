4e4888c2cf8d549591d263b07d10479ac6431149
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-02-15 23:03:46 -0300

    feat: add route delete user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { CreateUserController } from "@/controllers/useCases/createUser/CreateController"
 import { Router } from "express"
 import { GetUserController } from "../controllers/useCases/getUsers/GetUsersController"
+import { DeleteUserController } from "@/controllers/useCases/deleteUser/DeleteUserController"
 
 const router = Router()
 
@@ -16,6 +17,14 @@ router.post("/register", async (req, res) => {
   const createUserController = new CreateUserController()
 
   const { body, statusCode } = await createUserController.handle(req)
+
+  return res.status(statusCode).json(body)
+})
+
+router.delete("/:id", async (req, res) => {
+  const deleteUserController = new DeleteUserController()
+
+  const { body, statusCode } = await deleteUserController.handle(req)
 
   return res.status(statusCode).json(body)
 })
